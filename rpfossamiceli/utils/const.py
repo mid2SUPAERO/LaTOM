5a44679034c78e27d34e4f890ffba8e9531eea12
afossa <Alberto.FOSSA@student.isae-supaero.fr>
2019-09-07 10:59:14 +0200

    reorganized as py pkg

---
+++ rpfossamiceli/utils/const.py
@@ -0,0 +1,7 @@
+"""
+@authors: Alberto FOSSA' Giuliana Elena MICELI
+
+Defines useful constants
+"""
+
+g0 = 9.80665
---
+++ setup.py
@@ -0,0 +1,22 @@
+"""
+@authors: Alberto FOSSA', Giuliana Elena MICELI
+"""
+
+import setuptools
+setuptools.setup(
+      
+      name = 'rpfossamiceli',
+      version = '0.1',
+      description = '',
+      author = 'Alberto FOSSA, Giuliana Elena MICELI',
+      
+      packages = setuptools.find_packages()
+      
+      #install_requires = ['numpy>=1.16.4',
+      #                    'spiceypy>=2.2.0',
+      #                    'scipy>=1.3.0', 
+      #                    'matplotlib>=3.1.0'],
+                          
+      # scripts = ['scripts/genesis.py'],
+
+      )
