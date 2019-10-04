c80d632798d9266cd9c10d8cbf9a2479980ade26
GiulianaMic <gem.giuliana.miceli@gmail.com>
2019-10-04 17:57:22 +0200

    Descent trajectories added

--- scripts/surrogate.py
+++ scripts/surrogate.py
@@ -6,29 +6,29 @@
 import numpy as np
 
 from rpfm.surrogate.surrogate_2d import TwoDimAscConstSurrogate, TwoDimAscVarSurrogate, TwoDimAscVToffSurrogate,\
-    TwoDimDescVertSurrogate
+    TwoDimDescVertSurrogate, TwoDimDescConstSurrogate, TwoDimDescVarSurrogate, TwoDimDescVLandSurrogate
 
 from rpfm.utils.primary import Moon
 
 # trajectory
-kind = 'c'
+kind = 'dc'
 moon = Moon()
 alt = 100e3  # final orbit altitude [m]
 theta = np.pi/2  # guessed spawn angle [rad]
-tof_asc = 500  # guessed time of flight (ascent) [s]
-tof_desc = (1000, 100)  # guessed time of flight (descent) [s]
+tof = 500  # guessed time of flight [s]
+tof_desc_2p = (1000, 100)  # guessed time of flight (descent with 2 phases) [s]
 t_bounds = None  # time of flight bounds [-]
-alt_p = 15e3  # perigee altitude (descent only) [m]
-alt_switch = 3e3  # switch altitude (descent only) [m]
-alt_safe = 5e3  # minimum safe altitude (ascent safe only) [m]
-slope = 10.  # slope of the constraint on minimum safe altitude (ascent safe only) [-]
+alt_p = 15e3  # perigee altitude (descent 2 phases only) [m]
+alt_switch = 3e3  # switch altitude (descent 2 phases only) [m]
+alt_safe = 5e3  # minimum safe altitude (ascent and descent safe only) [m]
+slope = 10.  # slope of the constraint on minimum safe altitude (ascent and descent safe only) [-]
 isp_lim = (250., 500.)  # specific impulse lower and upper limits [s]
 twr_lim = (1.1, 4.)  # initial thrust/weight ratio lower and upper limits [-]
 
 # NLP
 method = 'gauss-lobatto'
 segments_asc = 20
-segments_desc = (10, 10)
+segments_desc_2p = (10, 10)
 order = 3
 solver = 'IPOPT'
 
@@ -41,20 +41,29 @@ criterion = 'ese'
 train_method = 'KRG'
 nb_eval = 400
 
-if kind == 'c':
-    sm = TwoDimAscConstSurrogate(moon, isp_lim, twr_lim, alt, theta, tof_asc, t_bounds, method, segments_asc, order,
+if kind == 'ac':
+    sm = TwoDimAscConstSurrogate(moon, isp_lim, twr_lim, alt, theta, tof, t_bounds, method, segments_asc, order,
                                  solver, nb_samp, samp_method=samp_method, criterion=criterion)
-elif kind == 'v':
+elif kind == 'av':
     sm = TwoDimAscVarSurrogate(moon, isp_lim, twr_lim, alt, t_bounds, method, segments_asc, order, solver, nb_samp,
                                samp_method=samp_method, criterion=criterion)
-elif kind == 's':
+elif kind == 'as':
     sm = TwoDimAscVToffSurrogate(moon, isp_lim, twr_lim, alt, alt_safe, slope, t_bounds, method, segments_asc, order,
                                  solver, nb_samp, samp_method=samp_method, criterion=criterion)
-elif kind == 'd':
-    sm = TwoDimDescVertSurrogate(moon, isp_lim, twr_lim, alt, alt_p, alt_switch, theta, tof_desc, t_bounds, method,
-                                 segments_desc, order, solver, nb_samp, samp_method=samp_method, criterion=criterion)
+elif kind == 'd2p':
+    sm = TwoDimDescVertSurrogate(moon, isp_lim, twr_lim, alt, alt_p, alt_switch, theta, tof_desc_2p, t_bounds, method,
+                                 segments_desc_2p, order, solver, nb_samp, samp_method=samp_method, criterion=criterion)
+elif kind == 'dc':
+    sm = TwoDimDescConstSurrogate(moon, isp_lim, twr_lim, alt, alt_p, theta, tof, t_bounds, method, segments_asc, order,
+                                  solver, nb_samp, samp_method=samp_method, criterion=criterion)
+elif kind == 'dv':
+    sm = TwoDimDescVarSurrogate(moon, isp_lim, twr_lim, alt, t_bounds, method, segments_asc, order, solver, nb_samp,
+                                samp_method=samp_method, criterion=criterion)
+elif kind == 'ds':
+    sm = TwoDimDescVLandSurrogate(moon, isp_lim, twr_lim, alt, alt_safe, -slope, t_bounds, method, segments_asc,
+                                  order, solver, nb_samp, samp_method=samp_method, criterion=criterion)
 else:
-    raise ValueError('kind must be c, v, s or d')
+    raise ValueError('kind must be ac, av, as or d2p, dc, dv, ds')
 
 sm.sampling()
 
