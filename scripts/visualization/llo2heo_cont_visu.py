5d7c1f707715b265468077dc8c8c3422310632d1
afossa <alberto.fossa18@gmail.com>
2020-03-09 17:11:42 +0100

    added 2 new solutions

--- scripts/visualization/llo2heo_cont_visu.py
+++ scripts/visualization/llo2heo_cont_visu.py
@@ -5,7 +5,7 @@ from rpfm.utils.primary import Moon
 from rpfm.data.data import dirname
 
 # load saved data
-filename = 'test_continuation2.pkl'
+filename = 'llo2apo_cont_i2e-3.pkl'
 abspath = '/'.join([dirname, 'continuation', filename])
 tr = load(abspath)
 
@@ -41,4 +41,5 @@ print(f"Number of spirals: {nb_spirals:.12f}")
 print(f"Propellant fraction (excluding insertion): {(1 - tr.states[0][-1, -1]):.12f}")
 print(f"Propellant fraction (total): {(1 - tr.states[-1][-1, -1]):.12f}")
 
+print(tr)
 tr.plot()
